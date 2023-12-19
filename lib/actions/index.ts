1617727ccde1abae53d48fbeed855ed10e6f3b56
Sidak Marwah <sidakmarwah@gmail.com>
2023-12-20 00:38:59 +0530

    fix some error

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
@@ -43,6 +44,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
             { upsert: true, new: true }
         );
 
+        revalidatePath(`/product/${newProduct._id}`);
         redirect(`/product/${newProduct._id}`);
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
